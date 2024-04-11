b8bfad97e288b0ef9123c42c131b2ce6de8a5b22
David Bueno Ferreira Rodrigues <davidifscinfo20@gmail.com>
2024-04-11 09:13:12 -0300

    att

--- js/otimizacao.js
+++ js/otimizacao.js
@@ -1,17 +1,17 @@
 
 
-const loadVideo = async() => {
-    const movieView = document.getElementById('movie-view')
-    const video = document.createElement('iframe')
-    video.src = "https://www.youtube.com/embed/J776O3uXGAc"
-    video.title = "YouTube video player"
-    video.frameborder="0"
-    video.allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    video.allowFullscreen
-    movieView.appendChild(video)
-    console.log(video)
+const loadVideo = () => {
+
+            const btnMovie = document.querySelector('.btn-movie')
+            btnMovie.style.display = "none"
+
+            const movieView = document.getElementById('movie-view')
+            const video = document.createElement('iframe')
+            video.src = "https://www.youtube.com/embed/J776O3uXGAc"
+            video.title = "YouTube video player"
+            video.frameborder = "0"
+            video.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            video.allowFullscreen
+            movieView.appendChild(video)   
+   
 }
-
-
-
-loadVideo()
